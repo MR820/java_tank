342e1cc1a6441a4e367b728b32b839c7af9ac275
邢志伟 <xingzhiwei8924@yundasys.com>
2021-02-24 11:21:43 +0800

    singleton,strategy

---
+++ src/main/java/cn/org/wyxxt/v17/FireStrategy.java
@@ -0,0 +1,11 @@
+package cn.org.wyxxt.v17;
+
+/**
+ * @author xingzhiwei
+ * @createBy IntelliJ IDEA
+ * @time 2021/2/24 10:37 上午
+ * @email jsjxzw@163.com
+ */
+public interface FireStrategy {
+    void fire(Tank t);
+}
---
+++ src/main/java/cn/org/wyxxt/v17/PropertyMgr.java
@@ -0,0 +1,38 @@
+package cn.org.wyxxt.v17;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author xingzhiwei
+ * @createBy IntelliJ IDEA
+ * @time 2021/2/23 10:19 上午
+ * @email jsjxzw@163.com
+ */
+public class PropertyMgr {
+    static Properties props = new Properties();
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private PropertyMgr(){}
+
+    //int getInt(key)
+    //getString(key)
+
+
+
+    public static Object get(String key) {
+        if (props == null) return null;
+        return props.get(key);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PropertyMgr.get("initTankCount"));
+    }
+}
